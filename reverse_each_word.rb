b8d3d877bf7d249c3d2e13aa59aef8f941742fa1
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 20:51:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(sentence1)
   # sentence1.each do|sentence1|
   #   puts words.reverse
-  string.split.reverse.join(' ')
+  sentence1[] words = sentence1.split(" ");
+  array.reverse(words);
+  return sentence1.join(" ", words);
 
-
+  # string.split.reverse.join(' ')
 end
