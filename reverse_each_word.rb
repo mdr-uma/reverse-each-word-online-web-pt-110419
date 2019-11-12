c1184fc177b55f006db564a996e9aa453d5b18fe
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 21:24:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.each {|word| word.reverse }
+  new_array.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
