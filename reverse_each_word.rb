2dd234f17e76dca404e6d4a80781cefdc558196d
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 21:16:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,10 @@ def reverse_each_word(string)
       end
         new_sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
